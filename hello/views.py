cb535ee7e45d0199c79c133b4ba7269317c684eb
hectorbenitez <hbenitez@nearsoft.com>
2015-03-10 11:00:05 -0600

    testing COMMIT_HASH

--- hello/views.py
+++ hello/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from os import getenv
 
 from .models import Greeting
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello from Python!')
+    return HttpResponse('Hello from Python!' % getenv("COMMIT_HASH"))
 
 
 def db(request):
